c96d1abc4173f274b436687ab9da5495300c05c7
Thomas W <63552974+thomas-w12@users.noreply.github.com>
2023-03-03 19:45:06 +0100

    completed challenge in day 22: Project 2, part 3: GuessTheFlag

--- 03_Guess_the_Flag/GuessTheFlag/GuessTheFlag/ContentView.swift
+++ 03_Guess_the_Flag/GuessTheFlag/GuessTheFlag/ContentView.swift
@@ -1,15 +1,20 @@
 //
 
-
 import SwiftUI
 
 struct ContentView: View {
+    let maxAskedQuestions = 8
+    
     @State private var showingScore = false
+    @State private var showingFinalScore = false
     @State private var scoreTitle = ""
     
-    
     @State var countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Nigeria", "Poland", "Russia", "Spain", "UK", "US"].shuffled()
+    
     @State var correctAnswer = Int.random(in: 0...2)
+    @State var userSelection: Int = 0
+    @State var userScore: Int = 0
+    @State var questionsAsked: Int = 0
     
     var body: some View {
         ZStack {
@@ -17,7 +22,7 @@ struct ContentView: View {
                 .init(color: Color(red: 0.1, green: 0.2, blue: 0.45), location: 0.3),
                 .init(color: Color(red: 0.76, green: 0.15, blue: 0.26), location: 0.3),
             ], center: .top, startRadius: 200, endRadius: 400)
-                .ignoresSafeArea()
+            .ignoresSafeArea()
             
             VStack {
                 Spacer()
@@ -41,29 +46,22 @@ struct ContentView: View {
                     ForEach(0..<3) { number in
                         Button {
                             if number == correctAnswer {
-                                scoreTitle = "Correct"
+                                scoreTitle = "Correct!"
+                                userScore += 1
                             } else {
-                                scoreTitle = "Wrong"
+                                scoreTitle = "Wrong!"
                             }
-                            showingScore = true
+                            questionsAsked += 1
+                            showingFinalScore = questionsAsked == maxAskedQuestions ? true : false
+                            userSelection = number
+                            showingScore = !showingFinalScore
                         } label: {
                             Image(countries[number])
                                 .renderingMode(.original)
-                                .clipShape(Capsule())
+                                .clipShape(RoundedRectangle(cornerRadius: 30))
                                 .shadow(radius: 5)
                         }
-                        .alert(scoreTitle, isPresented: $showingScore) {
-                            Button("OK") {
-                                countries.shuffle()
-                                correctAnswer = Int.random(in: 0...2)
-                            }
-                        } message: {
-                            // To Do
-                        }
-                        
                     }
-                    
-                    
                 }
                 .frame(maxWidth: .infinity)
                 .padding(.vertical, 20)
@@ -71,21 +69,42 @@ struct ContentView: View {
                 .clipShape(RoundedRectangle(cornerRadius: 20))
                 Spacer()
                 Spacer()
-
-                Text("Score: To DO")
+                
+                Text("Score: \(userScore) / \(questionsAsked)")
                     .foregroundColor(.white)
                     .font(.title.bold())
                 
                 Spacer()
-
-                
             }
             .padding()
-
+        }
+        .alert(scoreTitle, isPresented: $showingScore ) {
+            Button("OK") {
+                countries.shuffle()
+                correctAnswer = Int.random(in: 0...2)
+            }
+        } message: {
+            if (userSelection != correctAnswer) {
+                Text("That's the flag of \(countries[userSelection]).")
+            }
             
         }
-       
-       
+        .alert("Game over", isPresented: $showingFinalScore) {
+            Button("Restart Game") {
+                reset()
+            }
+        } message: {
+            Text("Your final score is \(userScore) / \(questionsAsked)")
+        }
+        
+        
+    }
+    
+    func reset() {
+        countries.shuffle()
+        correctAnswer = Int.random(in: 0...2)
+        userScore = 0
+        questionsAsked = 0
     }
 }
 
