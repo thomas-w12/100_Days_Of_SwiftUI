bfcaf2ecbebd9a2c3c83beb39c805aec317cfe2e
Thomas W <63552974+thomas-w12@users.noreply.github.com>
2023-03-05 11:37:45 +0100

    refactored game logic: removed switch-statement, polished alerts

--- 05_Rock_Paper_Sissors/RockPaperSissors/RockPaperSissors/ContentView.swift
+++ 05_Rock_Paper_Sissors/RockPaperSissors/RockPaperSissors/ContentView.swift
@@ -1,5 +1,4 @@
-//
-
+// Rock, Paper, Scissor Game
 
 import SwiftUI
 
@@ -8,22 +7,19 @@ struct ContentView: View {
     private let gameOptions = ["Rock", "Paper", "Scissors"]
     private let winOptions = [1,2,0]
     private let looseOptions = [2,0,1]
-    //  @State private var appPick: String = "Rock"
+    
     @State private var appPick: Int = Int.random(in: 0...2)
-
+    
     @State private var prompt: Bool = Bool.random()
     @State private var userCorrect: Bool = false
     @State private var showAlert = false
     @State private var rounds = 0
     @State private var userScore = 0
-    @State private var userPick: String = ""
+    @State private var userPick: Int = -1
     @State private var showScoreAlert: Bool = false
     
     
     var body: some View {
-        
-        
-        
         
         ZStack {
             
@@ -38,7 +34,6 @@ struct ContentView: View {
                     .fontWeight(.black)
                     .foregroundStyle(LinearGradient(gradient: Gradient(colors: [.teal, .orange]), startPoint: .trailing, endPoint: .leading))
                 
-               // Text(appPick)
                 Text(gameOptions[appPick])
                     .font(.largeTitle)
                     .frame(minWidth: 150)
@@ -62,42 +57,25 @@ struct ContentView: View {
                 
                 Spacer()
                 
-                ForEach(gameOptions, id: \.self) { a in
+                ForEach(0..<gameOptions.count, id: \.self) { a in
                     Button {
-                        
-                        switch a {
-                        case gameOptions[0]:
-                            //userCorrect = prompt && appPick == gameOptions[2] || !prompt && appPick == gameOptions[1]
-                            userCorrect = prompt && appPick == 2 || !prompt && appPick == 1
-                        case gameOptions[1]:
-                            //userCorrect = prompt && appPick == gameOptions[0] || !prompt && appPick == gameOptions[2]
-                            userCorrect = prompt && appPick == 0 || !prompt && appPick == 2
-                        case gameOptions[2]:
-                            //userCorrect = prompt && appPick == gameOptions[1] || !prompt && appPick == gameOptions[0]
-                            userCorrect = prompt && appPick == 1 || !prompt && appPick == 0
-                        default:
-                            userCorrect = false
-                        }
+                        userCorrect = prompt && a == winOptions[appPick] || !prompt && a == looseOptions[appPick]
                         userScore = userCorrect ? userScore+1 : userScore
                         userPick = a
                         rounds += 1
                         showScoreAlert = rounds == 10
                         showAlert = !showScoreAlert
-                        print("test")
                     } label: {
-                        Text(a)
-                        
+                        Text(gameOptions[a])
                             .font(.largeTitle)
                             .frame(minWidth: 150)
                             .fontWeight(.bold)
                             .padding(30)
                             .foregroundColor(.white.opacity(0.8))
-                            .background(showAlert ? (userPick == a ? (userCorrect ? .green : .red) : .gray.opacity(0.4)) : .gray.opacity(0.4))
+                            .background((showAlert || showScoreAlert) ? (userPick == a ? (userCorrect ? .green : .red) : .gray.opacity(0.4)) : .gray.opacity(0.4))
                             .clipShape(RoundedRectangle(cornerRadius: 30))
-                        
                     }
                     .buttonStyle(.plain)
-                    
                 }
                 Spacer()
                 
@@ -111,21 +89,16 @@ struct ContentView: View {
             }
             .alert(userCorrect ? "Correct!" : "Wrong!", isPresented: $showAlert) {
                 Button {
-                    prompt = Bool.random()
+                    prompt.toggle()
                     appPick = Int.random(in: 0...2)
                     showAlert = false
                 } label: {
-                    Text("OK ")
+                    Text("OK")
                 }
-                
-                
-            } message: {
-                Text("Test")
-                
             }
-            .alert("Final Score", isPresented: $showScoreAlert) {
+            .alert("Game Over!", isPresented: $showScoreAlert) {
                 Button {
-                    prompt = Bool.random()
+                    prompt.toggle()
                     appPick = Int.random(in: 0...2)
                     userScore = 0
                     rounds = 0
@@ -133,9 +106,8 @@ struct ContentView: View {
                 } label: {
                     Text("Restart Game")
                 }
-                
             } message: {
-                Text("Test")
+                Text("Your final score is: \(userScore) / \(rounds)")
             }
             
         }
