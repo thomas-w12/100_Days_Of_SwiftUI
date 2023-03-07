673d10444246b032d989e0813ff932f12e7efb70
Thomas W <63552974+thomas-w12@users.noreply.github.com>
2023-03-07 20:32:20 +0100

    tried some new animations

--- 08_Animations/Animations/Animations/ContentView.swift
+++ 08_Animations/Animations/Animations/ContentView.swift
@@ -4,57 +4,67 @@
 import SwiftUI
 
 struct ContentView: View {
-
     @State private var colors: [Color] = [.pink, .blue, .green, .yellow, .orange, .red]
-    @State private var colors2: [Color] = [.pink, .blue, .green, .yellow, .orange, .red, .pink, .blue, .green, .yellow, .orange, .red]
+    @State private var colors2: [Color] = [.pink, .blue, .green, .yellow, .orange, .red, .black, .white]
+    @State private var background: Color = .black
     
-    @State private var animationAmount = 0.0
-    @State private var backgroundColor: Color = .white
-    @State private var scaleEffect = 1.0
+    @State private var dragAmount = CGSize.zero
+    @State private var enabled = false
+    @State private var hasTimeElapsed = false
     
     var body: some View {
+        
         ZStack {
             
-            LinearGradient(colors: colors2, startPoint: .topLeading, endPoint: .bottomTrailing)
-                .edgesIgnoringSafeArea(.all)
-                .scaleEffect(scaleEffect)
+            background
+                .ignoresSafeArea(.all)
             
-            Button("Tap me") {
-                withAnimation(.easeOut(duration: 0.7)) {
-                    animationAmount += 360
-                    
-                    
-                    
-                }
-                
-                withAnimation(.easeInOut(duration: 0.1).repeatForever()) {
-                    scaleEffect = 3.5
-                }
-                colors2.shuffle()
-
-//                for color in colors {
-//                    withAnimation(.easeOut(duration: 0.3)) {
-//                        backgroundColor = color
-//
-//                    }
-//                }
-                
-            }
-            .foregroundColor(.primary)
-            .padding(50)
-            .background(AngularGradient(colors: colors, center: .center))
-            .clipShape(Circle())
-            .shadow(color: .pink, radius: 20)
-            .rotation3DEffect(.degrees(animationAmount), axis: (1, 1, 0))
             
-               
             
-           
+            LinearGradient(gradient: Gradient(colors: colors), startPoint: .topLeading, endPoint: .bottomTrailing)
+                .frame(width: 300, height: 200)
+                .clipShape(RoundedRectangle(cornerRadius: 10))
+                .offset(dragAmount)
+            // .shadow(color: colors.randomElement() ?? .black ,radius: 50)
+                .gesture(
+                    DragGesture()
+                        .onChanged { value in
+                            withAnimation {
+                                dragAmount = value.translation
+                                colors.shuffle()
+                                
+                            }
+                        }
+                        .onEnded({ value in
+                            withAnimation(.interpolatingSpring(stiffness: 20, damping: 10)) {
+                                dragAmount = CGSize(width: 0, height: 0)
+                                
+                            }
+                        })
+                )
+        }
+        .onAppear {
+            
         }
         
-                
+        
+        
+        
+        
+        
+        
+        
     }
+    
+
+    
 }
+
+
+
+
+
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
