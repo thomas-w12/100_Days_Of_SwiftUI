e7415a1f0bd570bae4385859c8ecc7fca2f0cb76
Thomas W <63552974+thomas-w12@users.noreply.github.com>
2023-03-18 23:16:12 +0100

    created new project and completed learnings on day 39

---
+++ 11_Moonshot/Moonshot/Moonshot/ContentView.swift
@@ -0,0 +1,24 @@
+//
+
+
+import SwiftUI
+
+
+struct ContentView: View {
+    
+    
+    var body: some View {
+        
+        
+        
+        
+        
+        
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ 11_Moonshot/Moonshot/Moonshot/MoonshotApp.swift
@@ -0,0 +1,13 @@
+//
+
+
+import SwiftUI
+
+@main
+struct MoonshotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
